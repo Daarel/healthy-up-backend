42038d542329fa464223a1e73fa5385358ee54b3
Fatillah <daarel.fatillah@outlook.com>
2026-05-28 23:08:41 +0700

    add documentation, update profile endpoint, and level up endpoint

--- backend/src/routes/userRoutes.js
+++ backend/src/routes/userRoutes.js
@@ -1,16 +1,20 @@
 import express from 'express';
 
-import {
-  deleteProfile,
-  getAllUsers,
-  getUserProfile,
-} from '../controllers/userController.js';
+// Menggunakan import Class sesuai arsitektur yang baru
+import UserController from '../controllers/userController.js';
 import { adminOnly, protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
 
-router.get('/all-users', adminOnly, getAllUsers);
+router.get('/all-users', protect, adminOnly, UserController.getAllUsers);
+
 router.use('/user', protect);
-router.route('/user').get(getUserProfile).delete(deleteProfile);
+
+router.route('/user')
+  .get(UserController.getUserProfile)
+  .delete(UserController.deleteProfile);
+
+router.patch('/user/picture', UserController.updatePicture);
+router.post('/user/level-up', UserController.levelUp);
 
 export default router;
