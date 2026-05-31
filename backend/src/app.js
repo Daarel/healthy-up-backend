5e040e2c93912a7800e76b2666909a195c520800
Fatillah <daarel.fatillah@outlook.com>
2026-05-31 15:29:01 +0700

    separete server and app file

---
+++ backend/src/app.js
@@ -0,0 +1,42 @@
+import cors from 'cors';
+import express from 'express';
+import helmet from 'helmet';
+import path from 'path';
+import swaggerUi from 'swagger-ui-express';
+import yaml from 'yamljs';
+
+import authRoutes from './routes/authRoutes.js';
+import healthProfileRoutes from './routes/healthProfileRoutes.js';
+import userProfileRoutes from './routes/userRoutes.js';
+
+const app = express();
+
+// security and middleware
+app.use(helmet());
+app.use(
+  cors({
+    origin: 'http://localhost:5173',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH'],
+    credentials: true,
+  }),
+);
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// api routes
+app.use('/api/v1/auth', authRoutes);
+app.use('/api/v1/health-profiles', healthProfileRoutes);
+app.use('/api/v1/users', userProfileRoutes);
+
+// documentation
+const swaggerDocument = yaml.load(
+  path.join(process.cwd(), 'docs', 'swagger.yaml'),
+);
+
+app.get('/', (req, res) => {
+  res.redirect('/api-docs');
+});
+
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+export default app;
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,46 +1,10 @@
-import cors from 'cors';
 import { config } from 'dotenv';
-import express from 'express';
-import helmet from 'helmet';
-import path from 'path';
-import swaggerUi from 'swagger-ui-express';
-import yaml from 'yamljs';
 
-import authRoutes from './routes/authRoutes.js';
-import healthProfileRoutes from './routes/healthProfileRoutes.js';
-import userProfileRoutes from './routes/userRoutes.js';
+import app from './app.js';
 
 config();
-const app = express();
+
 const PORT = process.env.PORT || 5001;
-
-// body parsing middleware
-app.use(helmet());
-app.use(
-  cors({
-    origin: 'http://localhost:5173',
-    methods: ['GET', 'POST', 'PUT', 'DELETE'],
-    credentials: true
-  }),
-);
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-// API routes
-app.use('/api/v1/auth', authRoutes);
-app.use('/api/v1/health-profiles', healthProfileRoutes);
-app.use('/api/v1/users', userProfileRoutes);
-
-// API documentation
-const swaggerDocument = yaml.load(
-  path.join(process.cwd(), 'docs', 'swagger.yaml'),
-);
-
-app.get('/', (req, res) => {
-  res.redirect('/api-docs');
-});
-
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.listen(PORT, () => {
   console.log(`Server running on PORT ${PORT}`);
