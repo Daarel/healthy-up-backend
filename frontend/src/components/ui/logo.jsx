6cd91c480a069cf134570b3ebf964bd5c93bac7f
Fatillah <daarel.fatillah@outlook.com>
2026-06-03 19:44:02 +0700

    Update gitignore to exclude frontend folder

--- frontend/src/components/ui/logo.jsx
+++
@@ -1,7 +0,0 @@
-export default function Logo({ className = "text-xl font-extrabold text-[#005823] font-lexend leading-none" }) {
-  return (
-    <span className={className}>
-      HealthyUp
-    </span>
-  );
-}
